e6522457d705d9962404e3acac78344b94241e50
Gourav1901 <gouravchoudhary2211@gmail.com>
2024-10-11 16:54:14 +0530

    update hamburger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,19 @@
-// import "../styles/navbar.css";
+import { useState } from "react";
 import style from "../styles/navbar.module.css";
 import pdf from "../pages/Images/GouravChoudhary-FullStackWebDeveloper-JoN9.pdf";
-export default function Navbar() {
-  return (
-    //--
-    //--
 
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
     <nav className="navbar navbar-expand-lg navSec ps-xl-0 ">
       <div
         id="nav-menu"
@@ -17,87 +25,81 @@ export default function Navbar() {
         >
           <span className={style.span}>G</span>ourav{""}
           <span>
-            <img
-              className="ms-lg-2 ms-md-2 ms-2 "
-              //src
-              // src="https://camo.githubusercontent.com/beb64ff21c883e318e4f5db5231c2ba4175705bea1c9249e82a41ab375db4f75/68747470733a2f2f6d65646961322e67697068792e636f6d2f6d656469612f51737347456d706b79454f684243623765312f67697068792e6769663f6369643d656366303565343761306e336769316266716e74716d6f62386739616964316f796a327772336473336d67373030626c267269643d67697068792e676966"
-              width="35"
-            />
+            <img className="ms-lg-2 ms-md-2 ms-2 " width="35" />
           </span>
         </a>
+
         <button
           className={`navbar-toggler ${style.hamburger}`}
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          aria-expanded={isOpen}
+          onClick={toggleMenu}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div
-          className="collapse navbar-collapse justify-content-end"
+          className={`collapse navbar-collapse justify-content-end ${isOpen ? style.show : ""
+            }`}
           id="navbarNavAltMarkup"
         >
-          <div className="navbar-nav align-items-center ">
+          <div className="navbar-nav align-items-center">
             <a
               className="nav-link home active me-lg-2 me-xl-5  text-info-emphasis "
-              aria-current="page"
               href="#home"
+              onClick={closeMenu}  // Closes menu on link click
             >
               HOME
             </a>
             <a
               className="nav-link about active me-lg-2 me-xl-5  text-info-emphasis "
-              aria-current="page"
               href="#about"
+              onClick={closeMenu}  // Closes menu on link click
             >
               ABOUT
             </a>
             <a
               className="nav-link skills active me-lg-2 me-xl-5 text-info-emphasis "
-              aria-current="page"
               href="#skills"
+              onClick={closeMenu}  // Closes menu on link click
             >
               SKILLS
             </a>
             <a
               className="nav-link projects active me-lg-2 me-xl-5  text-info-emphasis"
-              aria-current="page"
               href="#projects"
+              onClick={closeMenu}  // Closes menu on link click
             >
               PROJECTS
             </a>
             <a
               className="nav-link active me-lg-2 me-xl-5 text-info-emphasis"
-              aria-current="page"
               href="#github"
+              onClick={closeMenu}  // Closes menu on link click
             >
               GITHUB
             </a>
             <a
               className="nav-link contact active me-lg-2 me-xl-5  text-info-emphasis"
-              aria-current="page"
               href="#contact"
+              onClick={closeMenu}  // Closes menu on link click
             >
               CONTACT
             </a>
             <a
-               className="nav-link resume active   me-5 text-info-emphasis"
-               id="resume-link-1"
-               aria-current="page"
-               onClick={() =>
-                 window.open(
-                   "https://drive.google.com/file/d/1Cq2YiJ18DQLYlBqWb7uO5DHYIaiKv1VH/view?usp=sharing"
-                 )
-               }
-               download="Gourav Choudhary-Full Stack Web Developer"
-               href={pdf}
-             >
-               <button id="resume-button-1"  className={`${style.button} ms-5`}>
-                 <span className={style.buttonContent}>Resume </span>
-               </button>
+              className="nav-link resume active   me-5 text-info-emphasis"
+              onClick={() => {
+                window.open(
+                  "https://drive.google.com/file/d/1Cq2YiJ18DQLYlBqWb7uO5DHYIaiKv1VH/view?usp=sharing"
+                );
+                closeMenu(); // Closes menu when the resume link is clicked
+              }}
+              download="Gourav Choudhary-Full Stack Web Developer"
+              href={pdf}
+            >
+              <button id="resume-button-1" className={`${style.button} ms-5`}>
+                <span className={style.buttonContent}>Resume</span>
+              </button>
             </a>
           </div>
         </div>
